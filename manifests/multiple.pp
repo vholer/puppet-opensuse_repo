e88db02d9637306a4b653f0d4307b471fe8d40e1
Vlastimil Holer <holer@ics.muni.cz>
2014-08-29 12:28:57 +0200

    Use hiera_array merge lookup for opensuse_repo::multiple::repos param

--- manifests/multiple.pp
+++ manifests/multiple.pp
@@ -1,7 +1,10 @@
 class opensuse_repo::multiple (
-  $repos   = [],
+  $repos   = hiera_array('opensuse_repo::multiple::repos', []),
   $options = {},
   $stage   = setup
 ) {
+  validate_array($repos)
+  validate_hash($options)
+
   ensure_resource('opensuse_repo',$repos,$options)
 }
